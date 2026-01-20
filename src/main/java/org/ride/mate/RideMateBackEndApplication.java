44c9f587f375474e89a938a4a61f829d1d7c1297
TishanGamage <tishan.gamage555@gmail.com>
2026-01-20 22:50:38 +0530

    initial commit

---
+++ src/main/java/org/ride/mate/RideMateBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.ride.mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideMateBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideMateBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ride/mate/RideMateBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ride.mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideMateBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
