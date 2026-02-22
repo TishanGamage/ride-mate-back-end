a19d29a848ae6d848a501a8ccf8cb2131a138ddd
TishanGamage <tishan.gamage555@gmail.com>
2026-02-22 11:25:44 +0530

    added domains and repos

--- src/main/java/com/ride/mate/repository/DriverProfileRepository.java
+++ src/main/java/com/ride/mate/repository/DriverProfileRepository.java
@@ -1,9 +1,11 @@
 package com.ride.mate.repository;
 
 import com.ride.mate.domain.DriverProfile;
+import com.ride.mate.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -17,12 +19,21 @@ import java.util.Optional;
  * # Date       Story Point    Task No      Author           Description
  * ---------------------------------------------------------------------------
  * 1 20-02-2026    N/A          N/A          Tishan          Initial Development
+ * 2 22-02-2026    N/A          N/A          Tishan          Updated for User-based schema
  */
 @Repository
 public interface DriverProfileRepository extends JpaRepository<DriverProfile, Long> {
 
-    Optional<DriverProfile> findByUserProfileId(Long userProfileId);
+    Optional<DriverProfile> findByUser(User user);
+
+    Optional<DriverProfile> findByUserId(Long userId);
 
     Optional<DriverProfile> findByDriverLicenseNumber(String driverLicenseNumber);
+
+    List<DriverProfile> findByAccountStatus(String accountStatus);
+
+    boolean existsByUserId(Long userId);
+
+    boolean existsByDriverLicenseNumber(String driverLicenseNumber);
 }
 
