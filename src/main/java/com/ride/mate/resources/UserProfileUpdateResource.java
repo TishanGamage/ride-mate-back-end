90faca219bf5f35c9ee7f324bfd7dea235b77d69
TishanGamage <tishan.gamage555@gmail.com>
2026-03-15 14:03:08 +0530

    updated

---
+++ src/main/java/com/ride/mate/resources/UserProfileUpdateResource.java
@@ -0,0 +1,62 @@
+package com.ride.mate.resources;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * UserProfileUpdateResource
+ * Request payload for updating a user profile
+ *
+ * @author Tishan
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * # Date       Story Point    Task No      Author           Description
+ * ---------------------------------------------------------------------------
+ * 1 15-03-2026    N/A          N/A          Tishan          Initial Development
+ */
+@Getter
+@Setter
+public class UserProfileUpdateResource {
+
+    @NotNull(message = "{invalid.value}")
+    private Long id;
+
+    @NotNull(message = "{invalid.value}")
+    private Long version;
+
+    private Long profileImageDocumentId;
+
+    private String dateOfBirth;
+
+    private String gender;
+
+    private String bio;
+
+    private String addressLine1;
+
+    private String addressLine2;
+
+    private String addressLine3;
+
+    private String addressLine4;
+
+    private String city;
+
+    private String state;
+
+    private String postalCode;
+
+    private String country;
+
+    private String preferredLanguage;
+
+    @Valid
+    private UserIdentificationDetailsRequestResource userIdentificationDetails;
+
+    @Valid
+    private UserEmergencyContactDetailsRequestResource emergencyContactDetails;
+}
+
