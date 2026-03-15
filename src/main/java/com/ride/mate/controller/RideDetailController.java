19bcca1d4fa5c4f77d8c0dfb1882c535431de1c4
IruniA <iruniamanda21@gmail.com>
2026-03-15 15:30:20 +0530

    save ride details

--- src/main/java/com/ride/mate/controller/RideDetailController.java
+++ src/main/java/com/ride/mate/controller/RideDetailController.java
@@ -2,30 +2,60 @@ package com.ride.mate.controller;
 
 import com.ride.mate.core.MessagePropertyBase;
 import com.ride.mate.domain.RideDetail;
-import com.ride.mate.service.IdentificationTypeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ride.mate.resources.RideDetailRequestResource;
+import com.ride.mate.resources.SuccessAndErrorDetailsResource;
+import com.ride.mate.service.RideDetailService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * RideDetail Controller
  * REST API endpoints for RideDetails
-
+ *
+ * @author Iruni
+ * @version 1.0.0
+ * @since 1.0.0
+ *
  * # Date       Story Point    Task No      Author           Description
  * ---------------------------------------------------------------------------
- * 1 15-03-2026    N/A          N/A          Iruni          Initial Development
+ * 1 15-03-2026    N/A          N/A          Iruni           Initial Development
  */
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/ride-details")
 @CrossOrigin(origins = "*")
 public class RideDetailController extends MessagePropertyBase {
 
-    @Autowired
-    private RideDetailService rideDetailService;
+    private final RideDetailService rideDetailService;
+    private final Environment environment;
 
+    public RideDetailController(RideDetailService rideDetailService,
+                               Environment environment) {
+        this.rideDetailService = rideDetailService;
+        this.environment = environment;
+    }
 
+    /**
+     * Create a new ride detail
+     *
+     * @param request RideDetailRequestResource containing ride information
+     * @return ResponseEntity with success response
+     */
+    @PostMapping
+    public ResponseEntity<?> createRideDetail(@Valid @RequestBody RideDetailRequestResource request) {
+        log.info("Received request to create ride detail for driver profile ID: {}", request.getDriverProfileId());
 
+        RideDetail rideDetail = rideDetailService.createRideDetail(request);
 
+        SuccessAndErrorDetailsResource response = new SuccessAndErrorDetailsResource();
+        response.setId(rideDetail.getId());
+        response.setMessages(environment.getProperty(RECORD_CREATED));
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
---
+++ src/main/java/com/ride/mate/repository/RideDetailRepository.java
@@ -0,0 +1,40 @@
+package com.ride.mate.repository;
+
+import com.ride.mate.domain.RideDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Ride Detail Repository
+ * Data access layer for driver_ride_detail table
+ *
+ * @author Iruni
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * # Date       Story Point    Task No      Author           Description
+ * ---------------------------------------------------------------------------
+ * 1 15-03-2026    N/A          N/A          Iruni           Initial Development
+ */
+@Repository
+public interface RideDetailRepository extends JpaRepository<RideDetail, Long> {
+
+    /**
+     * Find ride details by driver profile ID
+     *
+     * @param driverProfileId Driver profile ID
+     * @return List of ride details
+     */
+    List<RideDetail> findByDriverProfileId(Long driverProfileId);
+
+    /**
+     * Find ride details by status
+     *
+     * @param status Status
+     * @return List of ride details
+     */
+    List<RideDetail> findByStatus(String status);
+}
+
