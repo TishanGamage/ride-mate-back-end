7b08cb610086413ec2be46856040d3c51b7e6971
TishanGamage <tishan.gamage555@gmail.com>
2026-03-16 22:39:39 +0530

    added new driver profile api

---
+++ src/main/java/com/ride/mate/controller/DriverProfileController.java
@@ -0,0 +1,61 @@
+package com.ride.mate.controller;
+
+import com.ride.mate.core.MessagePropertyBase;
+import com.ride.mate.domain.DriverProfile;
+import com.ride.mate.resources.DriverProfileRequestResource;
+import com.ride.mate.resources.SuccessAndErrorDetailsResource;
+import com.ride.mate.service.DriverProfileService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * DriverProfileController
+ * REST API endpoints for driver profile management operations
+ *
+ * @author Tishan
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * # Date       Story Point    Task No      Author           Description
+ * ---------------------------------------------------------------------------
+ * 1 16-03-2026    N/A          N/A          Tishan          Initial Development
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/driver-profile")
+@CrossOrigin(origins = "*")
+public class DriverProfileController extends MessagePropertyBase {
+
+    private final DriverProfileService driverProfileService;
+    private final Environment environment;
+
+    public DriverProfileController(DriverProfileService driverProfileService, Environment environment) {
+        this.driverProfileService = driverProfileService;
+        this.environment = environment;
+    }
+
+    /**
+     * Save or update driver profile for a user
+     * Creates a new driver profile and vehicle details, or updates if one already exists
+     *
+     * @param userId  the ID of the user
+     * @param request driver profile request containing license and vehicle details
+     * @return ResponseEntity with saved driver profile ID and success message
+     */
+    @PostMapping(value = "/save/{userId}")
+    public ResponseEntity<SuccessAndErrorDetailsResource> saveDriverProfile(
+            @PathVariable Long userId,
+            @Valid @RequestBody DriverProfileRequestResource request) {
+        log.info("Received driver profile save request for user ID: {}", userId);
+        DriverProfile driverProfile = driverProfileService.saveDriverProfile(userId, request);
+        SuccessAndErrorDetailsResource response = new SuccessAndErrorDetailsResource();
+        response.setId(driverProfile.getId());
+        response.setMessages(environment.getProperty(RECORD_CREATED));
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
+
---
+++ src/main/java/com/ride/mate/service/DriverProfileService.java
@@ -0,0 +1,29 @@
+package com.ride.mate.service;
+
+import com.ride.mate.domain.DriverProfile;
+import com.ride.mate.resources.DriverProfileRequestResource;
+
+/**
+ * DriverProfileService
+ * Service interface for driver profile management operations
+ *
+ * @author Tishan
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * # Date       Story Point    Task No      Author           Description
+ * ---------------------------------------------------------------------------
+ * 1 16-03-2026    N/A          N/A          Tishan          Initial Development
+ */
+public interface DriverProfileService {
+
+    /**
+     * Save or update driver profile and vehicle details for a given user
+     *
+     * @param userId  the ID of the user
+     * @param request driver profile request containing license and vehicle details
+     * @return the saved DriverProfile entity
+     */
+    DriverProfile saveDriverProfile(Long userId, DriverProfileRequestResource request);
+}
+
