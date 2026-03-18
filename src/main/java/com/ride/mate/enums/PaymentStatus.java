c947ee9a851a923ed431e68576164c3b815bcb6b
DanushkaNan <danushkanan@lolctech.com>
2026-03-18 22:05:58 +0530

    Added Payment Related Enums.

---
+++ src/main/java/com/ride/mate/enums/PaymentStatus.java
@@ -0,0 +1,21 @@
+package com.ride.mate.enums;
+
+/**
+ * Payment Status Enum
+ * Represents the status of a payment transaction or driver earning
+ *
+ * @author Danushka
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * # Date       Story Point    Task No      Author           Description
+ * ---------------------------------------------------------------------------
+ * 1 18-03-2026    N/A          N/A          Danushka          Initial Development
+ */
+public enum PaymentStatus {
+    PENDING,
+    SUCCESS,
+    FAILED,
+    REFUNDED
+}
+
---
+++ src/main/java/com/ride/mate/enums/WithdrawalStatus.java
@@ -0,0 +1,20 @@
+package com.ride.mate.enums;
+
+/**
+ * Withdrawal Status Enum
+ * Represents the status of a driver withdrawal request
+ *
+ * @author Danushka
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * # Date       Story Point    Task No      Author           Description
+ * ---------------------------------------------------------------------------
+ * 1 18-03-2026    N/A          N/A          Danushka          Initial Development
+ */
+public enum WithdrawalStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
+
