7364d66baaf3517fb62dde4dc5ef83c17d13b8dc
TishanGamage <tishan.gamage555@gmail.com>
2026-03-29 11:12:09 +0530

    added marketing site stats

--- src/main/java/com/ride/mate/resources/MarketingSiteStatsResponse.java
+++ src/main/java/com/ride/mate/resources/MarketingSiteStatsResponse.java
@@ -2,13 +2,13 @@
  * MarketingSiteStatsResponse
  * DTO for marketing site statistics (counts of rides completed, active users, verified drivers)
  *
- * @author Copilot
+ * @author Tishan
  * @version 1.0.0
  * @since 1.0.0
  *
  * # Date       Story Point    Task No      Author           Description
  * ---------------------------------------------------------------------------
- * 1 29-03-2026    N/A          N/A          Copilot          Initial Development
+ * 1 29-03-2026    N/A          N/A          Tishan          Initial Development
  */
 package com.ride.mate.resources;
 
